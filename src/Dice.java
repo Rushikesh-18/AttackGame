b14682206b095a6024bb30eb7ea4e4639d576501
rushikesh pachpute <rushikeshpachpute13@gmail.com>
2024-06-30 12:25:46 +0530

    DiceRandomFetch

--- src/Dice.java
+++ src/Dice.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Dice {
     private static final int SIDES = 6;
     private Random random;
-
+    //Pick one Random
     public Dice() {
         this.random = new Random();
     }
