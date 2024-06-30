63f16f7c1ece578e3004dcd98f22441293955b3a
rushikesh pachpute <rushikeshpachpute13@gmail.com>
2024-06-30 12:20:45 +0530

    RandomDiceCountpick

--- src/dice.java
+++ src/dice.java
@@ -1,3 +1,14 @@
+import java.util.Random;
+
 public class Dice {
-    
+    private static final int SIDES = 6;
+    private Random random;
+
+    public Dice() {
+        this.random = new Random();
+    }
+
+    public int roll() {
+        return random.nextInt(SIDES) + 1;
+    }
 }
